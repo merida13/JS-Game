3c435543dd2135b7fd215d317e64482a5091e526
merida13 <diana.bermudez.101@knights.ucf.edu>
2016-10-20 15:22:09 -0400

    Tar restarts game
    The fieldtype tar now restarts the char.

--- code/game.js
+++ code/game.js
@@ -213,14 +213,20 @@ Player.prototype.moveX = function(step, level, keys) {
 };
 
 var gravity = 30;
-var jumpSpeed = 17;
-var playerYSpeed = 7;
+var jumpSpeed = 18;
+var playerYSpeed = 10;
 
 Player.prototype.moveY = function(step, level, keys) {
 	this.speed.y += step * gravity;;
 	var motion = new Vector(0, this.speed.y * step);
 	var newPos = this.pos.plus(motion);
 	var obstacle = level.obstacleAt(newPos, this.size);
+	var startPos = new Vector(5,10);
+	
+	if (obstacle === "tar") {
+		this.pos = startPos; 
+	}
+	
 	if (obstacle) {
 		if (keys.up && this.speed.y > 0)
 			this.speed.y = -jumpSpeed;
